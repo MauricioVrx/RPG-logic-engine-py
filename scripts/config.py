cfb5ca8c33520c5c5fbdf94146cf2552bf3959ff
Mauricio Villanueva <47935336+MauricioVrx@users.noreply.github.com>
2026-02-04 16:44:59 -0300

    Update config.py
    Short description

--- scripts/config.py
+++ scripts/config.py
@@ -8,6 +8,8 @@ DEFAULTS_ACTIONS = [3, 1, 1]
 
 FREE_ABILITY_POINTS = 4
 
+# Items CSV files
+# {folder:[csv_files]}
 ITEMS_CSV_FILES = {
     "equipment": ["armor", "weapon", "shield"],
     "item": ["adventuring_gear", "consumables"]
